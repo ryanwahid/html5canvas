d2ddaf5ea5d7f21bd2308799f3ebfa7cd676b13f
ryan wahid <ryanwahid@gmail.com>
2018-07-25 22:09:01 -0500

    created random circles with color

--- canvas.js
+++ canvas.js
@@ -41,30 +41,70 @@ c.stroke();
 
 
 
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dx = (Math.random() - 0.5) * 30;
-var dy = (Math.random() - 0.5) * 30;
-var radius = 30;
+function Circle(x, y, dx, dy, radius) {
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.radius = radius;
+
+	this.draw = function() {
+		c.beginPath();
+		c.arc (this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		c.strokeStyle = 'blue';
+		c.stroke();
+		c.fill();
+		this.color = Math.random()*550; 
+		c.fillStyle = 'hsl('+this.color+',100%,50%)';
+	}
+
+	this.update = function() {
+		if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+		    this.dx = -this.dx;
+		}
+
+		if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+		    this.dy = -this.dy;
+		}
+
+		this.x += this.dx;
+		this.y += this.dy;
+		this.draw();
+	}
+
+}	
+
+
+
+var circleArray = [];
+
+for (var i = 0; i < 100; i++) {
+	var radius = 30;
+	var x = Math.random() * (innerWidth -radius * 2) + radius;
+	var y = Math.random() * (innerHeight - radius *2) + radius;
+	var dx = (Math.random() - 0.5) * 7;
+	var dy = (Math.random() - 0.5) * 7;
+	
+	circleArray.push(new Circle(x, y, dx, dy, radius)); 
+
+}
+
+
 function animate() {
 	requestAnimationFrame(animate);
 	c.clearRect(0, 0, innerWidth, innerHeight);
+	
 
-	c.beginPath();
-	c.arc (x, y, 30, 0, Math.PI * 2, false);
-	c.strokeStyle = 'blue';
-	c.stroke();
+	for (var i = 0; i < circleArray.length; i++) {
+		circleArray[i].update();
+	}	
 
-	if (x + radius > innerWidth || x - radius < 0) {
-		dx = -dx;
-	}
+	// c.beginPath();
+	// c.arc (x, y, 30, 0, Math.PI * 2, false);
+	// c.strokeStyle = 'blue';
+	// c.stroke();
 
-	if (y + radius > innerHeight || y - radius < 0) {
-		dy = -dy;
-	}
 
-	x += dx;
-	y += dy;
 }
 
 animate();
