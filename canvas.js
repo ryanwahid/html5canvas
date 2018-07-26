835598b03cdb877c0f380873d0c5e32089bb2676
ryan wahid <ryanwahid@gmail.com>
2018-07-25 21:26:08 -0500

    animated 1st circle

--- canvas.js
+++ canvas.js
@@ -4,7 +4,7 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+/*c.fillStyle = 'rgba(255, 0, 0, 0.5)';
 c.fillRect(100, 100, 100, 100);
 c.fillStyle = 'rgba(0, 0, 255, 0.5)';
 c.fillRect(400, 100, 100, 100);
@@ -22,7 +22,7 @@ c.lineTo(300, 100)
 c.lineTo(400, 300);
 c.strokeStyle = "#fa34a2"
 c.stroke();
-
+*/
 // Arc/ Circle
 
 //c.beginPath();
@@ -30,11 +30,41 @@ c.stroke();
 //c.strokeStyle = 'blue';
 //c.stroke();
 
-for (var i = 0; i < 70; i++ ) {
+/*for (var i = 0; i < 70; i++ ) {
 	var x = Math.random() * window.innerWidth;
 	var y = Math.random() * window.innerHeight;
 	c.beginPath();
 	c.arc (x, y, 30, 0, Math.PI * 2, false);
 	c.strokeStyle = 'blue';
 	c.stroke();
+}*/
+
+
+
+var x = Math.random() * innerWidth;
+var y = Math.random() * innerHeight;
+var dx = (Math.random() - 0.5) * 30;
+var dy = (Math.random() - 0.5) * 30;
+var radius = 30;
+function animate() {
+	requestAnimationFrame(animate);
+	c.clearRect(0, 0, innerWidth, innerHeight);
+
+	c.beginPath();
+	c.arc (x, y, 30, 0, Math.PI * 2, false);
+	c.strokeStyle = 'blue';
+	c.stroke();
+
+	if (x + radius > innerWidth || x - radius < 0) {
+		dx = -dx;
+	}
+
+	if (y + radius > innerHeight || y - radius < 0) {
+		dy = -dy;
+	}
+
+	x += dx;
+	y += dy;
 }
+
+animate();
